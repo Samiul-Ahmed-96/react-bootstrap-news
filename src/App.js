e08e794fd87c1530060617fca199f2536b678184
Samiulahmedanik <84468942+SAANIK45@users.noreply.github.com>
2021-09-28 03:40:53 +0600

    display news

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { useEffect, useState } from 'react';
+import { Row } from 'react-bootstrap';
 import './App.css';
-import logo from './logo.svg';
+import News from './components/News/News';
 
 function App() {
+  const [news,setNews] = useState([])
+  
+  useEffect(()=>{
+    fetch('https://newsapi.org/v2/everything?q=Apple&from=2021-09-27&sortBy=popularity&apiKey=b7f6a138b6b842208123ce42949f8dc9')
+    .then(res => res.json())
+    .then(data => setNews(data.articles))
+  },[])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <h3>Latest News</h3>
+    <Row xs={1} md={4} className="g-4">
+      {
+        news.map(nw => <News news={nw}></News>)
+      }
+    </Row>
+
+      
+  </div>
   );
 }
 
